01d1f8bdf41b84a667717a88c1048d3cf05ac995
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2020-05-14 16:30:10 +0300

    add TODOs

--- server/Server.server.lua
+++ server/Server.server.lua
@@ -3,3 +3,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 require(ReplicatedStorage.Source:WaitForChild("InitializeEnvironment"))
 
 -- TODO: implement player loading bla bla
+
+-- TODO: make a gun manager for keeping track of weapons both server and client
+-- TODO: serialize data, make a network lib
+-- TODO: objects have :serialize method
+-- TODO: map loading (including terrain)
