90aeb097c2efa8dddce1a11af862347e34889d06
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2020-05-16 01:38:49 +0300

    config fix, add listenFor filter

--- shared/NetworkLib/init.lua
+++ shared/NetworkLib/init.lua
@@ -1,7 +1,6 @@
 local RunService = game:GetService("RunService")
 
 local Enums = shared.Enums
-local Emitter = require(shared.Common.Emitter)
 
 local remotes = _G.Path.Remotes
 
@@ -17,26 +16,36 @@ function NetworkLib:_listenHandler(ev, callback, listenFor)
 
     -- bad spaghetti that will handle server-client communication in one module
     if RunService:IsClient() then
-        signal = ev:connect(function(id, ...)
-            local receivedEnum = NetworkLib:_toEnum(id)
-            if not listenFor then
-                callback(receivedEnum,  ...)
-            elseif listenFor and receivedEnum == listenFor then
-                callback(...)
+        signal =
+            ev:connect(
+            function(id, ...)
+                local receivedEnum = NetworkLib:_toEnum(id)
+                if not listenFor then
+                    callback(receivedEnum, ...)
+                elseif listenFor and receivedEnum == listenFor then
+                    callback(...)
+                end
             end
-        end)
+        )
     elseif RunService:IsServer() then
-        signal = ev:connect(function(player, id, ...)
-            local receivedEnum = NetworkLib:_toEnum(id)
-            if not listenFor then
-                callback(player, receivedEnum,  ...)
-            elseif listenFor and receivedEnum == listenFor then
-                callback(player, ...)
+        signal =
+            ev:connect(
+            function(player, id, ...)
+                local receivedEnum = NetworkLib:_toEnum(id)
+                if not listenFor then
+                    callback(player, receivedEnum, ...)
+                elseif listenFor and receivedEnum == listenFor then
+                    callback(player, ...)
+                end
             end
-        end)
+        )
     end
 
     -- register the signal
+    NetworkLib._activeListeners[signal] = true
+
+    -- TODO: figure out how to handle disconnnects, observe behaviour if
+    -- disconnect makes signal nil or keeps reference?
 
     return signal
 end
@@ -49,5 +58,17 @@ function NetworkLib:listen(callback)
         return NetworkLib:_listenHandler(remotes.Signal.OnClientEvent, callback)
     elseif RunService:IsServer() then
         return NetworkLib:_listenHandler(remotes.Signal.OnServerEvent, callback)
+    end
+end
+
+---Listen for specific signals with their marked enumerator
+---@param enum userdata Enumerable to filter for
+---@param callback function Executed whenever a signal is received
+---@return userdata RbxScriptSignal
+function NetworkLib:listenFor(enum, callback)
+    if RunService:IsClient() then
+        return NetworkLib:_listenHandler(remotes.Signal.OnClientEvent, callback, enum)
+    elseif RunService:IsServer() then
+        return NetworkLib:_listenHandler(remotes.Signal.OnServerEvent, callback, enum)
     end
 end
