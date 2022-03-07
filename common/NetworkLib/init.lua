84d55f7551ed3a7605ddfb8205364adb13037553
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2022-03-07 16:38:38 +0200

    add tostring for serialize warning

--- common/NetworkLib/init.lua
+++ common/NetworkLib/init.lua
@@ -35,7 +35,7 @@ function NetworkLib:_autoSerialize(...)
         if typeof(value) == "table" and value["serialize"] then
             value = value:serialize()
         elseif typeof(value) == "table" then
-            warn("no serialize function on object: " .. value .. "\n" .. debug.traceback())
+            warn("no serialize function on object: " .. tostring(value) .. "\n" .. debug.traceback())
         end
 
         log(2, "SERIALIZE: ASSIGN", key, "=", value)
