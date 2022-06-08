f7b5efc7db6d8c59c44fe5d384f16438a22d79c2
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2022-06-08 03:08:24 +0300

    add more log tools (still placeholders)

--- common/Log.lua
+++ common/Log.lua
@@ -1,21 +1,28 @@
-local LOG_LEVEL = _G.LOG_LEVEL or 2
 
---TODO: load GUI if on client
+--TODO: load GUI if on client, print server stuff, organize better than the command line
 
 return function(context)
     local function log(level, ...)
+        local LOG_LEVEL = _G.LOG_LEVEL or 2
         assert(typeof(level) == "number", "log function must have number as first argument (got " .. typeof(level) .. ")")
         if LOG_LEVEL >= level then
             print("[" .. context .. "]:", ...)
         end
     end
-    
+
     local function logwarn(level, ...)
+        local LOG_LEVEL = _G.LOG_LEVEL or 2
         assert(typeof(level) == "number", "logwarn function must have number as first argument (got " .. typeof(level) .. ")")
         if LOG_LEVEL >= level then
             warn("[" .. context .. "]:", ...)
         end
     end
 
-    return log, logwarn
+    local function logdebug(...)
+        if (_G.DEBUG_LOG or 0) > 0 then
+            print("[DEBUG][" .. context .. "]:", ...)
+        end
+    end
+
+    return log, logwarn, logdebug
 end
