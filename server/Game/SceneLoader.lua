7d9dfaec46a297042555ab9e84f14374c93c452d
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2022-07-03 22:04:18 +0300

    idk clean up sceneloader

--- server/Game/SceneLoader.lua
+++ server/Game/SceneLoader.lua
@@ -48,16 +48,15 @@ function SceneLoader:_loadEntities(scene)
     -- TODO: support folders in folders in folders
     for _, entity in pairs(scene.Model.Entities:GetChildren()) do
         assert(entity:GetAttribute("Type") ~= nil, "entity " .. entity:GetFullName() .. " must have 'Type' attribute")
+
         local entityType = manager:resolveType(entity:GetAttribute("Type"))
-        if not entityType then continue end
 
-        local propsTable = entity:GetAttributes()
-        propsTable.Instance = entity
+        if entityType then
+            local propsTable = entity:GetAttributes()
+            propsTable.Instance = entity
 
-        manager:add(
-            entityType,
-            propsTable
-        )
+            manager:add(entityType,propsTable)
+        end
     end
 end
 
